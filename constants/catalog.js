fff34aebf5e3ccca2ef72520b3e2f7f337e8ad30
Vlad <vald.nazarov95@yandex.ru>
2024-11-12 20:45:07 +0300

    #main: добавил каталог товаров

---
+++ constants/catalog.js
@@ -0,0 +1,68 @@
+const CATALOG = [
+  {
+    id: "el1",
+    name: "Fender 523",
+    img: "https://i.ibb.co/wJ9nRqX/2izze8pf8jwg48gwgko4w00gc.jpg",
+    price: 0,
+  },
+  {
+    id: "el2",
+    name: "Gibson 46723",
+    img: "https://i.ibb.co/KXqFTV1/5q1p7f74rj0gkk0k0ggk00cg8.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Fender 621",
+    img: "https://i.ibb.co/HKBBNsC/5yrntbvi5h4w0wwws4kw40gs.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Cort 302",
+    img: "https://i.ibb.co/tHyV62r/7rvih9yzl5c8kgso0ccsss8g.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Ibanez 326",
+    img: "https://i.ibb.co/R0jP1JV/8fyazx9wkc08ccwssgsgo84go.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Ibanez 734",
+    img: "https://i.ibb.co/DwvrRfg/9xgzl5mftb4g8kgko0cg0sosw.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Squier 365",
+    img: "https://i.ibb.co/6J4v1T7/20q7zxnio0asc0gwkws0sco48.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Ibanez 723",
+    img: "https://i.ibb.co/3dP2MdF/21vi5495z4000skg048sko08c.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Squier 622",
+    img: "https://i.ibb.co/NN4kr9B/84t4xv49c1gc8c8ckss400880.png",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Epiphone 325",
+    img: "https://i.ibb.co/0sN2y0F/97sos5e0p6gwc448wsw8k40c8.jpg",
+    price: 0,
+  },
+  {
+    id: "el3",
+    name: "Fender 245",
+    img: "https://i.ibb.co/9ZtSpC9/dr4kk8dnfnk00g84kw0kgowsw.jpg",
+    price: 0,
+  },
+];
